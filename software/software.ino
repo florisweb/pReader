b61dc471884682ee121bb166d6327edaa821d7b7
florisweb <floris@florisweb.dev>
2025-04-03 20:46:35 +0200

    Started building the paging system and dynamic requests for seperate music and music-pages.

--- software/software.ino
+++ software/software.ino
@@ -20,22 +20,23 @@ display(GxEPD2_1330_GDEM133T91(/*CS=*/ 5, /*DC=*/ 17, /*RST=*/ displayResetPin, 
 
 
 
-void displayImage(uint8_t img[], int width, int arrLength) {
-  display.firstPage();
-  do
-  {
-    display.fillScreen(GxEPD_WHITE); // Clear the display
 
-    display.drawBitmap(0, 0, img, width, arrLength * 8 / width, GxEPD_BLACK); // Draw the image
-  }
-  while (display.nextPage());
-}
+enum UIPage {
+  HOME,
+  MUSIC
+};
+int musicPage_curMusicIndex = 0;
+int musicPage_curPageIndex = 0;
+UIPage curPage = HOME;
 
-
-
-
+int availableMusicCount = 0;
+String availableMusic_names[10];
+int availableMusic_pageCount[10];
 
 
+
+
+int musicImageRequestId = 0;
 int remoteImageLength = 0;
 int imageWidth = 0;
 int curImageBufferIndex = 0;
@@ -46,22 +47,54 @@ int summedYPos = 0;
 const int displayWidth = 680;
 
 
+void onMusicImageRequestResponse(DynamicJsonDocument message) {
+  String error = message["response"]["error"];
+  if (error && error != "null")
+  {
+    Serial.print("Error on requesting image:" );
+    Serial.println(error);
+    return;
+  }
 
+  remoteImageLength = message["response"]["dataLength"];
+  imageWidth = message["response"]["imageWidth"];
+  curImageBufferIndex = 0;
+  summedYPos = 0;
+  overlapBufferLength = 0;
+  musicImageRequestId = message["response"]["imageRequestId"];
+
+  Serial.print("Got imageAvailable message. DataLength: ");
+  Serial.print(remoteImageLength);
+  Serial.print(" image width: ");
+  Serial.println(imageWidth);
+  ConnectionManager.sendRequest(
+    String("getImageSection"),
+    String("{\"startIndex\":" + String(curImageBufferIndex) + ", \"sectionLength\":" + String(imageSectionLength) + ", \"imageRequestId\": " + String(musicImageRequestId) + "}"),
+    &onImageSectionResponse
+  );
+}
 void onImageSectionResponse(DynamicJsonDocument message) {
+  String error = message["response"]["error"];
+  if (error && error != "null")
+  {
+    Serial.print("Error on requesting image section:" );
+    Serial.println(error);
+    return;
+  }
+
   int startIndex = message["response"]["startIndex"];
   String imageData = message["response"]["data"];
   curImageBufferIndex = startIndex + imageSectionLength;
-  Serial.print("Got section:");
-  Serial.print(startIndex);
-  Serial.print(" - ");
-  Serial.println(curImageBufferIndex);
-
+  //  Serial.print("Got section:");
+  //  Serial.print(startIndex);
+  //  Serial.print(" - ");
+  //  Serial.println(curImageBufferIndex);
 
   if (curImageBufferIndex < remoteImageLength)
   {
     ConnectionManager.sendRequest(
       String("getImageSection"),
-      String("{\"startIndex\":" + String(curImageBufferIndex) + ", \"sectionLength\":" + String(imageSectionLength) + "}"),
+      String("{\"startIndex\":" + String(curImageBufferIndex) + ", \"sectionLength\":" + String(imageSectionLength) + ", \"imageRequestId\": " + String(musicImageRequestId) + "}"),
       &onImageSectionResponse
     );
   } else Serial.println("Finished fetching all parts.");
@@ -106,9 +139,6 @@ void onImageSectionResponse(DynamicJsonDocument message) {
     imageOverlapBuffer[i] = decoded[outputLength - overlapBufferLength + i];
   }
 
-  //  display.writeImage(fullSection, 0, yStart, imageWidth, rows, true);
-  //  if (yStart < displayWidth && summedYPos > displayWidth) display.display(true);
-
   display.drawImage(fullSection, 0, yStart, imageWidth, rows, true);
 }
 
@@ -121,48 +151,29 @@ void onMessage(DynamicJsonDocument message) {
   String packetType = message["type"];
   String error = message["error"];
 
+
   Serial.print("[OnMessage] Error: ");
   Serial.println(error);
   Serial.print("[OnMessage] type: ");
   Serial.println(packetType);
 
-  if (packetType == "setText")
+  if (packetType == "curState")
   {
-    const int imageWidth = message["data"]["imgWidth"];
-    Serial.print("[OnMessage] width: ");
-    Serial.println(imageWidth);
-    String imgData = message["data"]["base64"];
-    const char * text = imgData.c_str();
-
-    size_t outputLength;
-    uint8_t* decoded = base64_decode((const unsigned char *)text, strlen(text), &outputLength);
-    Serial.print("Length of decoded message: ");
-    Serial.println(outputLength);
-
-    Serial.printf("%.*s", outputLength, decoded);
-    for (int i = 0; i < outputLength; i++)
+    availableMusicCount = 0;
+    for (int i = 0; i < sizeof(availableMusic_pageCount) / sizeof(int); i++)
     {
-      Serial.println(decoded[i]);
+      availableMusic_pageCount[i] = message["data"]["availableMusic"][i]["pages"];
+      if (availableMusic_pageCount[i] == 0) break;
+      const char* musicName = message["data"]["availableMusic"][i]["name"].as<const char*>();
+      availableMusic_names[i] = String(musicName);
+      availableMusicCount = i;
+
+      Serial.print("Music available: ");
+      Serial.print(availableMusic_names[i]);
+      Serial.print(" (");
+      Serial.print(availableMusic_pageCount[i]);
+      Serial.println(")");
     }
-    displayImage(decoded, imageWidth, outputLength);
-
-  } else if (packetType == "imageAvailable")
-  {
-    remoteImageLength = message["data"]["dataLength"];
-    imageWidth = message["data"]["imageWidth"];
-    curImageBufferIndex = 0;
-    summedYPos = 0;
-    overlapBufferLength = 0;
-
-    Serial.print("Got imageAvailable message. DataLength: ");
-    Serial.print(remoteImageLength);
-    Serial.print(" image width: ");
-    Serial.println(imageWidth);
-    ConnectionManager.sendRequest(
-      String("getImageSection"),
-      String("{\"startIndex\":" + String(curImageBufferIndex) + ", \"sectionLength\":" + String(imageSectionLength) + "}"),
-      &onImageSectionResponse
-    );
   }
 }
 
@@ -219,6 +230,42 @@ void loop() {
       display.nextPage();
     } else if (ch == "c") {
       display.fillScreen(GxEPD_WHITE);
-    }
+    } else if (ch == "m") {
+      openPage(MUSIC);
+    } else if (ch == "p0") {
+      musicPage_curPageIndex = 0;
+    } else if (ch == "p1") {
+      musicPage_curPageIndex = 1;
+    } else if (ch == "p2") {
+      musicPage_curPageIndex = 2;
+    } 
   }
+}
+
+
+void openMusicPage(int _curMusicIndex) {
+  musicPage_curMusicIndex = min(_curMusicIndex, availableMusicCount);
+  openPage(MUSIC);
+}
+void loadMusicImage() {
+  ConnectionManager.sendRequest(
+    String("requestMusicImage"),
+    String("{\"musicName\":\"" + String(availableMusic_names[musicPage_curMusicIndex]) + "\", \"pageIndex\":" + String(musicPage_curPageIndex) + "}"),
+    &onMusicImageRequestResponse
+  );
+}
+
+
+
+void openPage(UIPage page) {
+  if (page == HOME)
+  {
+    Serial.println("open HOME");
+
+  } else if (page == MUSIC)
+  {
+    Serial.println("open MUSIC");
+    loadMusicImage();
+  }
+
 }

bf2cd546dfb64af8093e61278b41b36ecefa10ba
florisweb <floris@florisweb.dev>
2025-05-20 19:44:15 +0200

    Added support for reading notes from a remote server to be displayed on the PReader.

---
+++ musicConnector/noteServer.js
@@ -0,0 +1,73 @@
+import { parse } from 'node-html-parser';
+import fs from "fs";
+import { Client } from 'basic-ftp';
+const CachePath = 'cache/noteFile';
+
+let Config;
+export default class noteManager {
+	#connected = false;
+	client
+
+	constructor(_config) {
+		Config = _config;
+		this.client = new Client();
+		this.downloadNote();
+	}
+
+	sync() {
+		return this.downloadNote();
+	}
+
+	async downloadNote() {
+		try {
+	        await this.client.access(Config);
+
+	        let files =  await this.client.list(Config.notePath);
+	        let noteFile;
+	        for (let file of files)
+	        {
+	        	if (!file.name.includes(Config.noteNamePart)) continue;
+	        	noteFile = file;
+	        	break;
+	        }
+	        if (!noteFile) return console.log('Error: note could not be found.');
+
+	        let res = await this.client.downloadTo(CachePath, Config.notePath + '/' + noteFile.name);
+	        if (res.code !== 226) return console.log('Error: error while downloading note.');
+	        this.onNoteDownloaded();
+	    } catch (e) {
+	    	console.log('error', e);
+	    }
+	}
+	onNoteDownloaded() {
+		let file = fs.readFileSync(CachePath);
+		let HTMLTree = parse(file);
+		let body = HTMLTree.querySelector('body');
+		let lineElements = body.childNodes;
+
+		let sections = [];
+		let curSection;
+		for (let el of lineElements)
+		{
+			if (el.rawTagName !== 'p') continue;
+			let curEl = el;
+			while (curEl.childNodes[0])
+			{
+				curEl = curEl.childNodes[0];
+			}
+
+			let text = curEl?._rawText;
+			if (!text) continue;
+			if (text.substr(0, 1) !== '[') // Header
+			{
+				if (curSection) sections.push(curSection);
+				curSection = {name: text, content: []};
+			} else {
+				if (!curSection) curSection = {name: 'pre', content: []};
+				curSection.content.push(text);
+			}
+		}
+		if (curSection) sections.push(curSection);
+		console.log(sections);
+	}
+}
--- musicConnector/server.js
+++ musicConnector/server.js
@@ -1,13 +1,13 @@
 import SocketServer from './webSocketServer.js';
-
+import noteServer from './noteServer.js';
 import musicInterface from './musicInterface.js';
 import { readFile } from './polyfill.js';
 
 let Config = await readFile('./config.json');
+const NoteServer = new noteServer(Config.NotesAPI);
+setInterval(() => NoteServer.sync(), Config.NotesAPI.updateFrequency);
 
-
-
-// MUSIC API
+// // MUSIC API
 const MusicInterface = new musicInterface(Config.MusicAPI, (_state) => {
 	console.log('change', _state);
 	SocketServer.onMusicStateChange(_state);
